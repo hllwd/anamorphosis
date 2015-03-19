a84a266a2f5b786da4fabe8daec8a7b9d513cd57
hllwd <nicolasmondon@gmail.com>
2015-03-19 22:11:53 +0100

    - raycaster : coords in setFromCamera needs to be normalized

--- web_modules/main.js
+++ web_modules/main.js
@@ -20,15 +20,22 @@ animate();
 document.getElementsByTagName('body')[0].addEventListener('mouseup', function (ev) {
     // @see http://stackoverflow.com/questions/11534000/three-js-converting-3d-position-to-2d-screen-position
     var p = new THREE.Vector3(0, 100, 100);
+
     //var raycaster = new THREE.Raycaster();
+    // v is normalized in Normalized Device Coordinates
+    // vector needs to be normalized
+    // http://threejs.org/docs/#Reference/Core/Raycaster
     var v = p.project(camera);
-    v.x = (v.x + 1) / 2 * size.w;
-    v.y = -(v.y - 1) / 2 * size.h;
-    // @see https://github.com/mrdoob/three.js/issues/5587
-    v.z = .5;
-    console.log(v);
+
+    // if you want to retrive reel coords
+    //v.x = (v.x + 1) / 2 * size.w;
+    //v.y = -(v.y - 1) / 2 * size.h;
+
+    // ??? @see https://github.com/mrdoob/three.js/issues/5587
+    //v.z = .5;
+
     var raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera( v, camera );
+    raycaster.setFromCamera(v, camera );
     console.log(raycaster.ray)
 
 });
